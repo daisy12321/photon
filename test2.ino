a27643a70f559450dfabb2c90fd647f925548136
Ying Zhuo <zhuo@dhcp-18-189-31-197.dyn.mit.edu>
2017-04-22 17:33:19 -0400

    the button can toggle, but still needs to press for a long time

--- test2.ino
+++ test2.ino
@@ -1,16 +1,13 @@
 int led = D7; // LED is connected to D0
 int pushButton = D2; // Push button is connected to D2
-int analogvalue = 0;
-double tempC = 0;
-char *message = "my name is particle";
-String aString;
-
+int pbs = LOW;
 
 // This routine runs only once upon reset
 void setup()
 {
   pinMode(led, OUTPUT); // Initialize D0 pin as output
   pinMode(pushButton, INPUT_PULLUP);
+  Particle.variable("pbs", LOW);
   /*Particle.subscribe("hook-response/pushButtonState", myHandler, MY_DEVICES);*/
   // Initialize D2 pin as input with an internal pull-up resistor
 }
@@ -18,25 +15,21 @@ void setup()
 // This routine loops forever
 void loop()
 {
-  int pushButtonState;
 
-  pushButtonState = digitalRead(pushButton);
+  pbs = digitalRead(pushButton);
 
-  if(pushButtonState == LOW){ //If we push down on the push button
+  if(pbs == LOW){ //If we push down on the push button
     digitalWrite(led, HIGH);  // Turn ON the LED
     /*Spark.publish("pushButtonState","Pressed",60,PRIVATE);*/
-    Particle.publish("pushButtonState","Pressed", 60, PRIVATE);
+    Particle.publish("pushButtonState","Pressed", 60);
     // Add a delay to prevent getting tons of emails from IFTTT
     delay(5000);
   }
   else
   {
-    Particle.publish("pushButtonState","Unpressed", 60, PRIVATE);
     digitalWrite(led, LOW);   // Turn OFF the LED
+    /*Particle.publish("pushButtonState","Unpressed", 60);*/
+
   }
 
-}
-
-void myfunction(int params) {
-  digitalWrite(led, HIGH);
 }
